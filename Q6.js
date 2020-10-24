fb8f3db24c470864c3eca622fbd804389cd83444
shatakshi <shatakshi.sharma@charpixel.com>
2020-10-24 14:16:36 +0530

    understing git commands

--- Q6.js
+++ Q6.js
@@ -12,16 +12,16 @@ var myObject = {
 };
 myObject.func();
 
-//Output:-
-"bar"
-"bar"
-undefined
-"bar"
-Explanation:-
-This is basically a scope game. Here func belongs to myObject ,and if we want to access the elements of object we use this function, like this.foo(foo variable belongs to myObject).
-.And var self=this;(means we assigning value to self variable, if we wouln't have assigned it,then second inner function to invoking function ,would also be undefined)
-DOUBT:-
-What i think is self and this are the same ,why did you use it twice?
-console.log("outer func:  this.foo = " + this.foo);
-console.log("outer func:  self.foo = " + self.foo);
-if they are different ,then how?
+// //Output:-
+// "bar"
+// "bar"
+// undefined
+// "bar"
+// Explanation:-
+// This is basically a scope game. Here func belongs to myObject ,and if we want to access the elements of object we use this function, like this.foo(foo variable belongs to myObject).
+// .And var self=this;(means we assigning value to self variable, if we wouln't have assigned it,then second inner function to invoking function ,would also be undefined)
+// DOUBT:-
+// What i think is self and this are the same ,why did you use it twice?
+// console.log("outer func:  this.foo = " + this.foo);
+// console.log("outer func:  self.foo = " + self.foo);
+// if they are different ,then how?
